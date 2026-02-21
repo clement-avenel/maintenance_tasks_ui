b2f7b1f25a65e7e6197b741cdd9d1be3b4c42629
Clément Avenel <contact@clement-avenel.com>
2026-02-21 22:53:07 +0100

    fix dummy app

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -43,11 +43,8 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Replace the default in-process memory cache store with a durable alternative.
-  # config.cache_store = :mem_cache_store
-
-  # Replace the default in-process and non-durable queuing backend for Active Job.
-  # config.active_job.queue_adapter = :resque
+  config.cache_store = :memory_store
+  config.active_job.queue_adapter = :inline
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
